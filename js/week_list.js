b79e0a76608fd77e385fef3535693b745729a273
Howiezhang226 <zhanghaonenu@gmail.com>
2016-05-08 11:14:25 -0400

    8: define global data 19: render default week 260: render variable and render function TO Do: change x-

--- js/week_list.js
+++ js/week_list.js
@@ -5,9 +5,10 @@ var weekNum = 25;
 // d3.json("json/recipient.json", function(error, result) {
 //     renderRecipList(result);
 // })
-
+var mainChartData = []
 d3.json("json/js_week.json", function(error, result) {
     data = result;
+    mainChartData = result;
     var filteredData = data.filter(function(d) {
         return d.days.length > 0;
     });
@@ -15,6 +16,7 @@ d3.json("json/js_week.json", function(error, result) {
         
     renderWeekList(filteredData);
     renderRecipList(filteredData);
+    renderMainChart(mainChartData, weekNum);
 })
 
 d3.json("json/jeff_2001_10.json", function(error, result) {
@@ -192,6 +194,7 @@ function renderWeekList(data) {
             weekNum = d.week_number;
             d3.select("#timeFrame").html(d.start_date + " - " + d.end_date);
             renderRecipList(data);
+            renderMainChart(mainChartData, weekNum);
         });
 
     var weekSvg = weekLi.append("svg")
@@ -254,7 +257,139 @@ function renderRecipList(data) {
 
 
 
+//global variable for main chart. Render only once.
+
+var margin = {top: 30, right: 50, bottom: 20, left: 30},
+  width = 700 - margin.left - margin.right,
+  height = 350 - margin.top - margin.bottom,
+  gridSize = Math.floor(height / 8),
+  days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"],
+  times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"],
+  gridWeight = gridSize * 2 / 3;
+var svg = d3.select("#chart1").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
+var barChart = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var circleChart = svg.append("g").attr("transform", "translate(" + gridWeight * 24.8 + "," + margin.top + ")");
+var dayLabels = barChart.selectAll(".dayLabel")
+  .data(days)
+  .enter().append("text")
+  .text(function (d) {
+      return d;
+  })
+  .attr("x", 0)
+  .attr("y", function (d, i) {
+      return i * gridSize;
+  })
+  .style("text-anchor", "end")
+  .attr("transform", "translate(-6," + gridSize + ")")
+  .attr("class", function (d, i) {
+      return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis");
+  });
+
+var timeLabels = barChart.selectAll(".timeLabel")
+  .data(times)
+  .enter().append("text")
+  .text(function (d) {
+      return d;
+  })
+  .attr("x", function (d, i) {
+      return i * gridWeight;
+  })
+  .attr("y", 0)
+  .style("text-anchor", "middle")
+  .attr("transform", "translate(" + gridSize / 3 + ", " + (height) + ")")
+  .attr("class", function (d, i) {
+      return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis");
+  });
+
+var renderMainChart = function (data, week) {
+    var dataFiltered = data.filter(function (d) {
+        return d.week_number == week;
+    });
+    var results = [];
+    var circles = [];
+    for (var i = 0; i < dataFiltered[0].days.length; i++) {
+        var day_data = dataFiltered[0].days;
+        //console.log(day_data[i].length);
 
 
+        var day = day_data[i]["day_num"];
+        var circle_result = {};
+        circle_result["day"] = day;
+        circle_result["total"] = day_data[i]["total"];
+        circles.push(circle_result);
+        var myMap = day_data[i]["daytime"];
+        for (var key in myMap) {
+            var result = {};
+            result["day"] = day;
+            result["hour"] = key;
+            result["value"] = myMap[key];
+            results.push(result);
+        }
 
+
+    }
+    console.log(results.length);
+    console.log(circles.length);
+    //console.log(circles);
+    var rectScale = d3.scale.linear()
+      .domain([0, 16])
+      .range([0, gridSize]);
+
+    var rScale = d3.scale.linear().range([5, gridSize / 2]).domain([0, 50]);
+    svg.selectAll(".hour").remove();
+    svg.selectAll("circle").remove();
+    var cards = barChart.selectAll(".hour")
+      .data(results, function (d) {
+          return d.day + ':' + d.hour;
+      });
+
+    //cards.append("title");
+
+    cards.enter().append("rect")
+      .attr("x", function (d) {
+          return (d.hour) * gridWeight;
+      })
+      .attr("y", function (d) {
+          return (d.day) * gridSize + gridSize - rectScale(d.value);
+      })
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .attr("class", "hour bordered")
+      .attr("width", gridWeight)
+      .attr("height", function (d) {
+            return rectScale(d.value);
+        }
+      )
+      .style("fill", "#225ea8");
+//
+//                    cards.transition().duration(1000)
+//                            .style("fill", function(d) { return colorScale(d.value); });
+
+    //cards.select("title").text(function(d) { return d.value; });
+    console.log(circles);
+    var selection = circleChart.selectAll("circle").data(circles, function (d) {
+        console.log(d.day + ':' + d.total);
+        return d.day + ':' + d.total;
+    });
+
+    selection.enter()
+      .append("circle")
+      .on("click", function (d, i) {
+          alert(d.day + ':' + d.total);
+      })
+      .attr("r", function (d, i) {
+          return rScale(d.total);
+      })
+      .attr("cx", 40)
+      .attr("cy", function (d, i) {
+          return (1 + parseInt(d.day) ) * gridSize;
+      })
+      .attr("fill", "#225ea8");
+
+    cards.exit().remove();
+    selection.exit().remove();
+
+};
 
